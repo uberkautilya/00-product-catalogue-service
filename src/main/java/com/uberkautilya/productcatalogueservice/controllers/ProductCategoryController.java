3de1f5c47559069039a5ac53f1ad6c9a22d6ce7e
Mithun <mithuncm@gmail.com>
2025-02-22 18:00:50 +0530

    Removed empty lines

--- src/main/java/com/uberkautilya/productcatalogueservice/controllers/ProductCategoryController.java
+++ src/main/java/com/uberkautilya/productcatalogueservice/controllers/ProductCategoryController.java
@@ -7,37 +7,31 @@ import java.util.List;
 
 @RestController(value = "/product-categories")
 public class ProductCategoryController {
-    //Get all
     @GetMapping
     public List<ProductCategoryDto> getAllProductCategories() {
         return null;
     }
 
-    //Get by id
     @GetMapping
     public ProductCategoryDto getProductCategoryById(Long id) {
         return null;
     }
 
-    //Create
     @PostMapping
     public ProductCategoryDto saveProductCategory(ProductCategoryDto productCategoryDto) {
         return null;
     }
 
-    //Update
     @PatchMapping
     public ProductCategoryDto updateProductCategory(Long id, ProductCategoryDto productCategoryDto) {
         return null;
     }
 
-    //Replace
     @PutMapping
     public ProductCategoryDto replaceProductCategory(Long id, ProductCategoryDto productCategoryDto) {
         return null;
     }
 
-    //Delete
     @DeleteMapping
     public boolean deleteProductCategory(Long id) {
         return false;
