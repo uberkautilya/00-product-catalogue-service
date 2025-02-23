4be8241cf017194fa143bedbc474d2d77b1133df
Mithun <mithuncm@gmail.com>
2025-02-23 23:27:17 +0530

    Refactor to check if the response code is 200 before mapping the DTO class.

--- src/main/java/com/uberkautilya/productcatalogueservice/services/FakeStoreProductService.java
+++ src/main/java/com/uberkautilya/productcatalogueservice/services/FakeStoreProductService.java
@@ -4,6 +4,7 @@ import com.uberkautilya.productcatalogueservice.dtos.FakeStoreProductDto;
 import com.uberkautilya.productcatalogueservice.models.Product;
 import com.uberkautilya.productcatalogueservice.models.ProductCategory;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -22,8 +23,12 @@ public class FakeStoreProductService implements IProductService {
         String url = "https://fakestoreapi.com/products/{id}";
         //Todo: id is the argument to the restTemplate method call
         ResponseEntity<FakeStoreProductDto> dtoResponseEntity = restTemplate.getForEntity(url, FakeStoreProductDto.class, id);
-        FakeStoreProductDto dto = dtoResponseEntity.getBody();
-        return from(dto);
+        if (dtoResponseEntity.getBody() != null &&
+                dtoResponseEntity.getStatusCode().equals(HttpStatusCode.valueOf(200))) {
+            FakeStoreProductDto dto = dtoResponseEntity.getBody();
+            return from(dto);
+        }
+        return null;
     }
 
     private Product from(FakeStoreProductDto dto) {
